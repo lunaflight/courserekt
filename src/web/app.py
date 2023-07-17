8c569a577f1f72fd52d2e91df7fc056163e4124d
et-irl <e0958283@u.nus.edu>
2023-07-18 03:25:44 +0800

    Made round links appear only if they exist

--- src/web/app.py
+++ src/web/app.py
@@ -25,19 +25,32 @@ def history():
 
     output = []
     error = None
+    round_exists = []
     try:
         output = get_all_data(year, semester, type)
+        for round_num in range(4):
+            round_exists.append(
+                    pdf_exists(f'{year}/{semester}/'
+                               f'{type}/round_{round_num}.pdf'))
     except ValueError as e:
         error = e
 
-    return render_template('history.html', output=output, error=error)
+    return render_template('history.html', output=output,
+                           round_exists=round_exists, error=error)
+
+
+pdf_directory = os.path.abspath(
+        os.path.join(os.path.dirname(__file__),
+                     '../coursereg_history/data/pdfs'))
+
+
+def pdf_exists(filename):
+    file_path = os.path.join(pdf_directory, filename)
+    return os.path.isfile(file_path)
 
 
 @app.route('/pdfs/<path:filename>')
 def serve_pdf(filename):
-    pdf_directory = os.path.abspath(
-            os.path.join(os.path.dirname(__file__),
-                         '../coursereg_history/data/pdfs'))
     return send_from_directory(pdf_directory, filename)
 
 
