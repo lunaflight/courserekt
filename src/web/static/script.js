4c5bc8b30145482b51dfaf37886da1fe46aaeb14
et-irl <e0958283@u.nus.edu>
2023-07-18 22:19:46 +0800

    Added a way to see course names, and toggle it.

--- src/web/static/script.js
+++ src/web/static/script.js
@@ -1,24 +1,42 @@
-let loadingSpinner = document.getElementById('loading-spinner'); // Get the loading spinner element
+const loadingSpinner = document.getElementById('loading-spinner'); // Get the loading spinner element
 
-function showLoadingSpinner() {
+async function showLoadingSpinner() {
   loadingSpinner.classList.remove('hidden');
 }
 
-function hideLoadingSpinner() {
+async function hideLoadingSpinner() {
   loadingSpinner.classList.add('hidden');
 }
 
-function filterCourses() {
-  let searchInput = document.getElementById('search-input');
-  let filter = searchInput.value.toUpperCase();
-  let tableRows = document.getElementsByTagName('tr');
+async function toggleColumnByCheckbox() {
+  const checkbox = document.getElementById('toggle-column-checkbox');
+  const isVisible = checkbox.checked;
 
-  let filters = filter.split(' ').filter(Boolean);
+  const table = document.getElementById("table-data");
+  const columnIndex = 1;
+
+  // Toggle the visibility of the column in the tbody
+  const tbodyCells = table.getElementsByTagName("tbody")[0].getElementsByTagName("td");
+  for (let i = columnIndex; i < tbodyCells.length; i += table.rows[0].cells.length) {
+    tbodyCells[i].classList.toggle("hidden", !isVisible);
+  }
+
+  // Toggle the visibility of the column in the thead
+  const theadCells = table.getElementsByTagName("thead")[0].getElementsByTagName("th");
+  theadCells[columnIndex].classList.toggle("hidden", !isVisible);
+}
+
+async function filterCourses() {
+  const searchInput = document.getElementById('search-input');
+  const filter = searchInput.value.toUpperCase();
+  const tableRows = document.getElementsByTagName('tr');
+
+  const filters = filter.split(' ').filter(Boolean);
 
   for (let i = 1; i < tableRows.length; i++) {
-    let courseCode = tableRows[i].getElementsByTagName('td')[0].textContent.toUpperCase();
+    const courseCode = tableRows[i].getElementsByTagName('td')[0].textContent.toUpperCase();
 
-    let isMatch = filters.some(filter => courseCode.indexOf(filter) > -1);
+    const isMatch = filters.some(filter => courseCode.indexOf(filter) > -1);
 
     if (filters.length == 0 || isMatch) {
       tableRows[i].classList.remove('hidden');
@@ -29,11 +47,16 @@ function filterCourses() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  let searchInput = document.getElementById('search-input');
+  const checkbox = document.getElementById('toggle-column-checkbox');
+
+  checkbox.addEventListener('change', () => toggleColumnByCheckbox());
+  toggleColumnByCheckbox();
+
+  const searchInput = document.getElementById('search-input');
   searchInput.addEventListener('input', filterCourses);
   filterCourses();
 
-  let courseForm = document.getElementById('course-form');
+  const courseForm = document.getElementById('course-form');
   courseForm.addEventListener('change', () => {
     courseForm.submit();
     showLoadingSpinner();
