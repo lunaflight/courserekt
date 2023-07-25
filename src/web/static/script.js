acc715370bd94cfce73a5b1ec2692247fa72a87c
et-irl <e0958283@u.nus.edu>
2023-07-25 08:00:00 +0800

    Cleaned and documented web code

--- src/web/static/script.js
+++ src/web/static/script.js
@@ -1,40 +1,70 @@
-const loadingSpinner = document.getElementById('loading-spinner'); // Get the loading spinner element
+const loadingSpinner = document.getElementById('loading-spinner');
 
+/**
+ * Show the loading spinner element.
+ * @async
+ * @function
+ */
 async function showLoadingSpinner() {
   loadingSpinner.classList.remove('hidden');
 }
 
+/**
+ * Hide the loading spinner element.
+ * @async
+ * @function
+ */
 async function hideLoadingSpinner() {
   loadingSpinner.classList.add('hidden');
 }
 
+/**
+ * Toggle the visibility of course title column in the table based on the checkbox state.
+ * @async
+ * @function
+ */
 async function toggleColumnByCheckbox() {
   const checkbox = document.getElementById('toggle-column-checkbox');
   const isVisible = checkbox.checked;
 
   const table = document.getElementById("table-data");
-  const columnIndex = 1;
+  const courseTitleIndex = 1;
 
-  // Toggle the visibility of the column in the tbody
+  // Toggle the visibility of the column in the table body
   const tbodyCells = table.getElementsByTagName("tbody")[0].getElementsByTagName("td");
-  for (let i = columnIndex; i < tbodyCells.length; i += table.rows[0].cells.length) {
+  for (let i = courseTitleIndex; i < tbodyCells.length; i += table.rows[0].cells.length) {
     tbodyCells[i].classList.toggle("hidden", !isVisible);
   }
 
-  // Toggle the visibility of the column in the thead
+  // Toggle the visibility of the column in the table headers
   const theadCells = table.getElementsByTagName("thead")[0].getElementsByTagName("th");
-  theadCells[columnIndex].classList.toggle("hidden", !isVisible);
+  theadCells[courseTitleIndex].classList.toggle("hidden", !isVisible);
 }
 
+/**
+ * Filter the courses in the table based on the input search text.
+ * Courses are shown if and only if they contain at least one of the search patterns
+ * as a substring in their course code.
+ *
+ * The search input can accept multiple search patterns delimited by spaces.
+ * For example, if the input is "CS3 GEC", the table will show all courses
+ * that have "CS3" or "GEC" in their course code.
+ *
+ * @async
+ * @function
+ */
 async function filterCourses() {
-  const searchInput = document.getElementById('search-input');
-  const filter = searchInput.value.toUpperCase();
+  const searchInput = document.getElementById('search-input').value.toUpperCase();
   const tableRows = document.getElementsByTagName('tr');
+  const filters = searchInput.split(' ').filter(Boolean);
 
-  const filters = filter.split(' ').filter(Boolean);
+  const courseCodeIndex = 0;
 
   for (let i = 1; i < tableRows.length; i++) {
-    const courseCode = tableRows[i].getElementsByTagName('td')[0].textContent.toUpperCase();
+    const courseCode = tableRows[i]
+      .getElementsByTagName('td')[courseCodeIndex]
+      .textContent
+      .toUpperCase();
 
     const isMatch = filters.some(filter => courseCode.indexOf(filter) > -1);
 
@@ -46,16 +76,23 @@ async function filterCourses() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+/**
+ * This code block sets up event listeners and initial state when the DOM content is loaded.
+ *
+ * @event
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  // The checkbox determines if the course title is shown.
   const checkbox = document.getElementById('toggle-column-checkbox');
-
-  checkbox.addEventListener('change', () => toggleColumnByCheckbox());
+  checkbox.addEventListener('change', toggleColumnByCheckbox);
   toggleColumnByCheckbox();
 
+  // The search bar should constantly filter courses as new input is received.
   const searchInput = document.getElementById('search-input');
   searchInput.addEventListener('input', filterCourses);
   filterCourses();
 
+  // The data should immediately update when new data is requested by a button press.
   const courseForm = document.getElementById('course-form');
   courseForm.addEventListener('change', () => {
     courseForm.submit();
