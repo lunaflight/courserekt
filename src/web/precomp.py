e0a11de80d7354dbb8ed1b0008c375aab07fdb1c
et-irl <e0958283@u.nus.edu>
2023-12-07 20:59:56 +0800

    Add documentation

--- src/web/precomp.py
+++ src/web/precomp.py
@@ -4,17 +4,11 @@ import os
 import shutil
 
 
-def main():
-    # List of years
+def main() -> None:
     years = ["2122", "2223", "2324"]
-
-    # List of semesters
     semesters = ["1", "2"]
-
-    # List of student types
     student_types = ["ug", "gd"]
 
-    # Generate all combinations
     combinations = itertools.product(years, semesters, student_types)
 
     try:
@@ -26,7 +20,16 @@ def main():
         generate_html(year, semester, student_type)
 
 
-def generate_html(year: str, semester: str, student_type: str):
+def generate_html(year: str, semester: str, student_type: str) -> None:
+    """
+    Generates an HTML file for a specific year, semester, and student type.
+    It then saves it in static/pages/{year}/{semester}/{student_type}.
+
+    Args:
+        year: The academic year (e.g., 2223).
+        semester: The semester (e.g., 1 or 2).
+        student_type: The student type (e.g., "ug" or "gd").
+    """
     client = app.test_client()
 
     data = {
@@ -36,7 +39,6 @@ def generate_html(year: str, semester: str, student_type: str):
     }
     response = client.post('/', data=data, follow_redirects=True)
 
-    # Extract the year, semester, and type from the request data
     year, semester, type = data['year'], data['semester'], data['type']
 
     # Construct the file path
@@ -48,8 +50,6 @@ def generate_html(year: str, semester: str, student_type: str):
     # Save the response text as an HTML file
     with open(file_path, "w") as f:
         f.write(response.text)
-
-    print(f"Saved HTML content to: {file_path}")
 
 
 if __name__ == '__main__':
