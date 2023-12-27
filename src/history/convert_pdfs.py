ac0410ff817462a407030f0647d432ab1f6169bb
lunaflight <e0958283@u.nus.edu>
2023-12-27 16:09:41 +0800

    Add PDF conversion progress monitoring

--- src/history/convert_pdfs.py
+++ src/history/convert_pdfs.py
@@ -3,12 +3,14 @@ import os
 import shutil
 
 from tabula.io import convert_into_by_batch
+from src.history.util.PdfCsvMonitorer import PdfCsvMonitorer
+
+TMP_DIRECTORY = "tmp_combined_pdfs"
 
 
 def convert(pdf_files: list[str]) -> None:
     # Combine all PDFs into a single directory
-    target_dir = "combined_pdfs"
-    os.makedirs(target_dir, exist_ok=True)
+    os.makedirs(TMP_DIRECTORY, exist_ok=True)
     for pdf_file in pdf_files:
         # Extract the directory path
         file_dir = os.path.dirname(pdf_file)
@@ -23,25 +25,29 @@ def convert(pdf_files: list[str]) -> None:
         new_filename = f"{encoded_dir}||{filename}{extension}"
 
         # Copy the file with the new name
-        shutil.copy2(pdf_file, os.path.join(target_dir, new_filename))
+        shutil.copy2(pdf_file, os.path.join(TMP_DIRECTORY, new_filename))
+
+    monitorer = PdfCsvMonitorer(TMP_DIRECTORY)
 
     # Run tabula conversion on the combined directory
+    monitorer.start()
     convert_into_by_batch(
-        target_dir,
+        TMP_DIRECTORY,
         output_format="csv",
         pages="all",
         lattice=True,
     )
+    monitorer.stop()
 
     # Remove all PDFs
-    for file in os.listdir(target_dir):
+    for file in os.listdir(TMP_DIRECTORY):
         # Check if the file extension is .pdf
         if file.endswith(".pdf"):
             # Delete the file
-            os.remove(os.path.join(target_dir, file))
+            os.remove(os.path.join(TMP_DIRECTORY, file))
 
     # Process each file in combined_pdfs directory
-    for file in os.listdir(target_dir):
+    for file in os.listdir(TMP_DIRECTORY):
         # Extract encoded directory and filename
         new_csv_file = "/".join(file.replace("pdfs", "raw").split("||"))
         new_file_dir = os.path.dirname(new_csv_file)
@@ -50,10 +56,10 @@ def convert(pdf_files: list[str]) -> None:
         os.makedirs(new_file_dir, exist_ok=True)
 
         # Move the file to the new location
-        shutil.move(os.path.join(target_dir, file), new_csv_file)
+        shutil.move(os.path.join(TMP_DIRECTORY, file), new_csv_file)
 
     # Delete the combined_pdfs directory
-    shutil.rmtree(target_dir)
+    shutil.rmtree(TMP_DIRECTORY)
 
 
 def main() -> None:
---
+++ src/history/util/PdfCsvMonitorer.py
@@ -0,0 +1,30 @@
+import os
+from threading import Thread
+from time import sleep
+
+
+class PdfCsvMonitorer(Thread):
+    def __init__(self, directory: str, update_freq: int = 1):
+        self.directory = directory
+        self.update_freq = update_freq
+
+        Thread.__init__(self)
+        self.running = True
+
+    def run(self) -> None:
+        prev_csv_cnt: int = -1
+
+        while self.running:
+            csv_files: list[str] = [file for file in os.listdir(self.directory) if file.endswith(".csv")]
+            pdf_files: list[str] = [file for file in os.listdir(self.directory) if file.endswith(".pdf")]
+            csv_files_cnt: int = len(csv_files)
+            pdf_files_cnt: int = len(pdf_files)
+
+            if prev_csv_cnt != csv_files_cnt:
+                print(f"Converting PDFs to CSVs... {csv_files_cnt}/{pdf_files_cnt}")
+
+            prev_csv_cnt = csv_files_cnt
+            sleep(1)
+
+    def stop(self) -> None:
+        self.running = False
